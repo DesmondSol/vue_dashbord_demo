bd33947046aa1b342a8779f0654f85512f36ab77
desmondsol <soltig66@gmail.com>
2022-04-25 18:12:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import headers from './components/header.vue'
+import footers from './components/footer.vue'
+</script>
+
+<template> 
+<headers /> 
+
+  <!--<headers msg="Hello Vue 3 + Vite" />-->
+  
+
+
+  
+
+  <footers />
+
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 0px;
+}
+</style>
